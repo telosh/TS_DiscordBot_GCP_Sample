bce01d65b0a18d02666b2d442ea003511ab348f9
telosh <terumi.contact.web@gmail.com>
2024-10-27 15:30:27 +0900

    fix

--- src/main.ts
+++ src/main.ts
@@ -7,9 +7,9 @@ import path from 'path';
 //render用にexpressでサーバーを立てる
 import express, { Request, Response } from 'express';
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 app.get('/', (req: Request, res: Response) => {
-    res.send('Hello World!');
+    res.send('Hello telosh World!');
 });
 
 app.listen(PORT);
