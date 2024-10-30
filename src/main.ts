d6ad3c5080d493416df00e116788ec3a261576b4
telosh <terumi.contact.web@gmail.com>
2024-10-30 22:46:25 +0900

    a

--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,10 @@ import registerCommands from './deploy-commands'; registerCommands();
 import express from 'express';
 const app = express();
 const PORT = process.env.PORT || 8080;
+app.get('/', (req, res) => {
+    res.send('Hello, Cloud Run!');
+  });
+  
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
